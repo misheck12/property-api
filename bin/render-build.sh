075d1eddd39ec05d7ae580558f4d5b547292a82a
('misheckcodes') <('mishecklivingi@live.com')>
2023-06-27 14:00:54 +0000

    Jun 27, 2023, 2:00 PM

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,16 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
 
-set -e
-
-# Install dependencies
 bundle install
-
-# Precompile assets
-bundle exec rake assets:precompile
-
-# Clean assets
-bundle exec rake assets:clean
-
-# Run database migrations
 bundle exec rake db:migrate
-
